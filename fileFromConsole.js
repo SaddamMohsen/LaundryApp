9c3611bf3e68afb9b81b4703a40d474a33cddae6
SaddamMohsen <sad.2000@outlook.com>
2021-04-29 20:48:24 +0300

    LaundryApp repository
    LaundryApp repository

---
+++ fileFromConsole.js
@@ -0,0 +1,19 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.4.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC7pysB9kIh4hyBkzwlZNvCHq7J_cNxs-Q",
+    authDomain: "wired-name-310722.firebaseapp.com",
+    projectId: "wired-name-310722",
+    storageBucket: "wired-name-310722.appspot.com",
+    messagingSenderId: "409742167764",
+    appId: "1:409742167764:web:782f77aed920369a3af541"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
